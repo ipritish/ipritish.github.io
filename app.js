bde470e9fd3f6c1c9fd934bcd85a84d674114f3c
ipritish <pritishjain2009@gmail.com>
2016-02-02 12:40:33 +0530

    first working commit

---
+++ app.js
@@ -0,0 +1,27 @@
+angular.module('resume', []);
+angular.module('resume')
+.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',function($stateProvider, $urlRouterProvider, $locationProvider) 
+{
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+
+    $stateProvider.state('bioData', {
+        url: '/bioData',
+        templateUrl: 'app/components/bioData/bio.html'
+    });
+    $stateProvider.state('resume', {
+        url: '/resume',
+        templateUrl: 'app/components/resume/resume.html'
+    });
+	$stateProvider.state('home', {
+        url: '/',
+        templateUrl: 'app/components/main/main.html'
+    });
+    /* Add New States Above */
+    $urlRouterProvider.otherwise('home');
+
+    $locationProvider.html5Mode(true);
+
+}]);
