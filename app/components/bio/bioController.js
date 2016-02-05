ef968c6a44d4be1341aa83a946b41171ebd58a86
ipritish <pritishjain2009@gmail.com>
2016-02-05 12:11:02 +0530

    navigation control

---
+++ app/components/bio/bioController.js
@@ -0,0 +1,7 @@
+angular.module('myInfo')
+.controller('bioController',['$state','$scope', function($state,$scope) 
+{
+
+	var bioController = this;
+
+}]);
---
+++ app/components/resume/resumeController.js
@@ -0,0 +1,6 @@
+angular.module('myInfo')
+.controller('resumeController',['$state','$scope', function($state,$scope) 
+{
+		var resumeController = this;
+
+}]);
