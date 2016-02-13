48cfe57347c5f083d4a330506edcc25cd5739791
ipritish <pritishjain2009@gmail.com>
2016-02-13 13:16:32 +0530

    details added

--- app/components/bio/bioController.js
+++ app/components/bio/bioController.js
@@ -1,6 +1,12 @@
 angular.module('myInfo')
 .controller('bioController',['$state','$scope', function($state,$scope) 
 {
-	$scope.details = [{"key":"test","detail" : "test"},
-					{"key" : "test2","detail" : "test2"}];
+	$scope.personalDetails = [{"key":"test","personalDetail" : "test"},
+							  {"key" : "test2","personalDetail" : "test2"}];
+					
+	$scope.familyDetails = [{"key":"test","familyDetail" : "test"},
+							{"key" : "test2","familyDetail" : "test2"}];
+							
+	$scope.communicationDetails = [{"key":"test","communicationDetail" : "test"},
+							{"key" : "test2","communicationDetail" : "test2"}];
 }]);
