4edcf5ec436e531a69b7fbf4715a3b08081f4340
ipritish <pritishjain2009@gmail.com>
2016-02-21 23:59:54 +0530

    resume styling completed

--- app/components/resume/resumeController.js
+++ app/components/resume/resumeController.js
@@ -25,5 +25,35 @@ angular.module('myInfo')
 									"institute":"IIT Gandhinagar",
 									"timeOfEducation":"June 2009 - May 2013"
 								}];
+	
+	$scope.firstProject = [
+							"tea","milk","coffee"
+						  ];
+						  
+	$scope.secondProject = [
+							"tea","milk","coffee"
+						   ];
+	$scope.thirdProject = [
+							"tea","milk","coffee"
+						  ];
+	$scope.fourthProject = [
+							"tea","milk","coffee"
+						   ];						  
+	
+	$scope.undergradeCourse = [
+								"tea","milk","coffee"
+						      ];	
+						   
+	$scope.techExperience = [
+								"tea","milk","coffee"
+							];
+	
+	$scope.additionalDetails = [
+								  "tea","milk","coffee"
+								];
+	$scope.techincalDetails = [
+								  "tea","milk","coffee"
+								];
+							
 
 }]);
